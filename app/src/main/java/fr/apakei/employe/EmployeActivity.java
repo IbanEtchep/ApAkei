b86af2ea0ba7326653cbfc64a3c327814b2bf0b9
Flloooow <florian.ganderatz@gmail.com>
2022-03-04 16:04:38 +0100

    ajout activities employe2

--- app/src/main/java/fr/apakei/employe/EmployeActivity.java
+++ app/src/main/java/fr/apakei/employe/EmployeActivity.java
@@ -1,4 +1,16 @@
 package fr.apakei.employe;
 
-public class EmployeActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.apakei.R;
+
+public class EmployeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_employe);
+    }
 }
--- app/src/main/java/fr/apakei/employe/RayonMagasinActivity.java
+++ app/src/main/java/fr/apakei/employe/RayonMagasinActivity.java
@@ -1,4 +1,16 @@
 package fr.apakei.employe;
 
-public class RayonMagasinActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import fr.apakei.R;
+
+public class RayonMagasinActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rayonmagasin);
+    }
 }
