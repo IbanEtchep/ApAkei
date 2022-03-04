0339e6b8338f14cefd743f6919c4470314f22af6
Iban <iban.etchep@gmail.com>
2022-03-04 16:51:22 +0100

    commit

--- app/src/main/java/fr/apakei/locations/LocationListActivity.java
+++ app/src/main/java/fr/apakei/locations/LocationListActivity.java
@@ -3,7 +3,17 @@ package fr.apakei.locations;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.Display;
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.apakei.Magasin;
+import fr.apakei.Modele;
 import fr.apakei.R;
 
 public class LocationListActivity extends AppCompatActivity {
@@ -12,5 +22,15 @@ public class LocationListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_list);
+
+        ListView listView = (ListView) findViewById(R.id.list_magasins);
+        List<Magasin> magasins = Modele.getLesMagasins();
+        List<String> magasinsNames = new ArrayList<>();
+        for (Magasin magasin : magasins) {
+            magasinsNames.add(magasin.getNom());
+        }
+        ArrayAdapter<String> magasinArrayAdapter = new ArrayAdapter<String>(this, R.layout.list_element, magasinsNames);
+        listView.setAdapter(magasinArrayAdapter);
+
     }
 }
