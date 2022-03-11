7db030dab724ff0feedc8ebeadcb5656a62283f7
julgndr16 <julegendre16@gmail.com>
2022-03-11 13:36:44 +0100

    boutton activity principal

--- app/src/main/java/fr/apakei/produits/ListRayonsActivity.java
+++ app/src/main/java/fr/apakei/produits/ListRayonsActivity.java
@@ -11,5 +11,9 @@ public class ListRayonsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_rayons);
+
+
+
+
     }
 }
